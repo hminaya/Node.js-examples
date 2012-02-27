7366d718ebd14a76a0e1ee4185e2a202b814a5b8
Hector Minaya <hminaya@gmail.com>
2012-02-27 03:37:57 +0000

    'from c9.io'

--- webserver.js
+++ webserver.js
@@ -4,6 +4,7 @@ var h = http.createServer(function (req, res) {
 	
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.write('Hello\n');
+    
 	setTimeout(function(){
 		res.end('world\n');
 	},2000);
